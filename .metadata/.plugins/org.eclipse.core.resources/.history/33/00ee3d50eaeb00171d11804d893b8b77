55383d7a74957a467ae18f32d33cddd7fa7f0dc5
srinath <srinathreddy.200@gmail.com>
2017-12-28 11:19:36 -0500

    added interface program

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/33/00ee3d50eaeb00171d11804d893b8b77
@@ -0,0 +1,13 @@
+package com.srinath.Interfaces;
+
+public class TestingInterface {
+
+	public static void main(String[] args) {
+		
+		Pilot1 pilot = new Pilot1();
+		pilot.changeFlaps(4);
+		pilot.increaseSpeed(40);
+		
+	}
+
+}
---
+++ TekGroup_practice/src/com/srinath/Interfaces/AirPlane.java
@@ -0,0 +1,8 @@
+package com.srinath.Interfaces;
+
+public interface AirPlane {     //interfaces are abstract by default.
+	
+	void increaseSpeed(int a);   //methods are by default public and abstract.
+	void changeFlaps(int b);
+	
+}
